d5b7b1b3251e26db8d664cb94c6f2e52388b6983
tz01x <abdur963rahman@gmail.com>
2022-03-09 07:07:56 +0600

    initial commit

---
+++ logbill/initial_settings.py
@@ -0,0 +1,15 @@
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIRS = [BASE_DIR/'templates',BASE_DIR/'build']
+STATIC_URL = '/static/'
+MEDIA_URL ='/media/'
+MEDIA_ROOT = BASE_DIR/'media'
+STATIC_ROOT = BASE_DIR/'static'/'static_root'/'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR/ "static" / "static_dir",
+    BASE_DIR/ "frontend" / "build"/"static",
+]
